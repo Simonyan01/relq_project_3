6e494c9d13db79ab2876db6945ccf9fef5a1a6b9
Simonyan01 <lyova.simonyan.01@mail.ru>
2022-09-02 17:47:32 +0400

    Changed

--- src/Login.js
+++ src/Login.js
@@ -7,17 +7,17 @@ const Login = () => {
 
     const [popupStyle, showPopup] = useState("hide")
     const [username, setusername] = useState("");
-    const [password, setpassword] = useState(""); 
+    const [password, setpassword] = useState("");
 
     const users = ["admin", "admin123"];
 
     const authenticate = (e) => {
         e.preventDefault()
-        
-        if (username===users[0] && password===users[1]) {
+
+        if (username === users[0] && password === users[1]) {
             navigate("/Menu");
         } else popup();
-      };
+    };
 
     const popup = () => {
         showPopup("login-popup")
@@ -29,18 +29,17 @@ const Login = () => {
         <div className="main">
             <div className="cover">
                 <h1 className="login-page">Login Page</h1>
-<<<<<<< HEAD
-                <input className="L_input" type="text" placeholder="Username"  />
+                <input className="L_input" type="text" placeholder="Username" />
                 <input className="L_input" type="password" placeholder="Password" />
 
                 <div className="login-btn" onClick={() => popup()}> Login </div>
-=======
-                <input className="L_input" type="text" placeholder="Username" value={username} onChange={(event)=>setusername(event.target.value)}/>
-                
-                <input className="L_input" type="password" placeholder="Password" value={password} onChange={(event)=>setpassword(event.target.value)}/>
+
+                <input className="L_input" type="text" placeholder="Username" value={username} onChange={(event) => setusername(event.target.value)} />
+
+                <input className="L_input" type="password" placeholder="Password" value={password} onChange={(event) => setpassword(event.target.value)} />
 
                 <div className="login-btn" onClick={authenticate}> Login </div>
->>>>>>> 8cdf5aa9b2689daf8a92d1f6c9e8a4f8ac710e5e
+
 
                 <div className={popupStyle}>
                     <h2>Login Failed</h2>
